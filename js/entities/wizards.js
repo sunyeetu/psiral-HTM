835ceb4e5b5fb7cad913788e44383f06c635a72f
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-11-29 00:32:01 +0100

    Updated fire wizard animations

--- js/entities/wizards.js
+++ js/entities/wizards.js
@@ -328,6 +328,16 @@ game.FireWizardEntity = game.WizardEntity.extend({
         // setup props
         this.name = 'Valeriya';
 
+        // {!} EXPR
+        this.renderable.addAnimation('stand_left', [0]);
+        this.renderable.addAnimation('stand_right', [6]);
+        this.renderable.addAnimation('stand_up', [0]);
+        this.renderable.addAnimation('stand_down', [0]);
+        this.renderable.addAnimation('walk_up', [0, 1, 2, 3, 4, 5]);
+        this.renderable.addAnimation('walk_left', [12, 13, 14, 15, 16, 17]);
+        this.renderable.addAnimation('walk_down', [0, 1, 2, 3, 4, 5]);
+        this.renderable.addAnimation('walk_right', [6, 7, 8, 9, 10, 11]);                
+
         this.playAnimation('stand_left');
     }
 });
