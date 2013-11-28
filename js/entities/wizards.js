d532bcb248c1e301ff5140196e594e0ff1a1bdf9
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-11-29 00:48:37 +0100

    Updated air wizar animations.

--- js/entities/wizards.js
+++ js/entities/wizards.js
@@ -42,10 +42,11 @@ game.WizardEntity = me.ObjectEntity.extend({
         this.renderable.addAnimation('stand_right', [0]);
         this.renderable.addAnimation('stand_up', [0]);
         this.renderable.addAnimation('stand_down', [0]);
-        this.renderable.addAnimation('walk_up', [0, 1, 2, 3, 4, 5]);
-        this.renderable.addAnimation('walk_left', [0, 1, 2, 3, 4, 5]);
+        // this.renderable.addAnimation('walk_up', [0, 1, 2, 3, 4, 5]);
+        this.renderable.addAnimation('walk_up', [6, 7, 8, 9, 10, 11], 75);
+        this.renderable.addAnimation('walk_left', [12, 13, 14, 15, 16, 17]);
         this.renderable.addAnimation('walk_down', [0, 1, 2, 3, 4, 5]);
-        this.renderable.addAnimation('walk_right', [0, 1, 2, 3, 4, 5]);
+        this.renderable.addAnimation('walk_right', [6, 7, 8, 9, 10, 11]);
 
         this.renderable.addAnimation('spellcast_left', [0, 1, 2, 3]);
         this.renderable.addAnimation('spellcast_right', [0, 1, 2, 3]);
@@ -280,16 +281,6 @@ game.EarthWizardEntity = game.WizardEntity.extend({
         // setup props
         this.name = 'Entria-Sil';
         
-        // {!} EXPR
-        this.renderable.addAnimation('stand_left', [0]);
-        this.renderable.addAnimation('stand_right', [6]);
-        this.renderable.addAnimation('stand_up', [0]);
-        this.renderable.addAnimation('stand_down', [0]);
-        this.renderable.addAnimation('walk_up', [0, 1, 2, 3, 4, 5]);
-        this.renderable.addAnimation('walk_left', [12, 13, 14, 15, 16, 17]);
-        this.renderable.addAnimation('walk_down', [0, 1, 2, 3, 4, 5]);
-        this.renderable.addAnimation('walk_right', [6, 7, 8, 9, 10, 11]);
-
         this.playAnimation('stand_right');
     }
 });
@@ -302,17 +293,7 @@ game.WaterWizardEntity = game.WizardEntity.extend({
         this.parent(x, y, settings);
 
         // setup props
-        this.name = 'Azalsor';
-
-        // {!} EXPR
-        this.renderable.addAnimation('stand_left', [0]);
-        this.renderable.addAnimation('stand_right', [6]);
-        this.renderable.addAnimation('stand_up', [0]);
-        this.renderable.addAnimation('stand_down', [0]);
-        this.renderable.addAnimation('walk_up', [0, 1, 2, 3, 4, 5]);
-        this.renderable.addAnimation('walk_left', [12, 13, 14, 15, 16, 17]);
-        this.renderable.addAnimation('walk_down', [0, 1, 2, 3, 4, 5]);
-        this.renderable.addAnimation('walk_right', [6, 7, 8, 9, 10, 11]);        
+        this.name = 'Azalsor';  
 
         this.playAnimation('stand_down');        
     }
@@ -326,17 +307,7 @@ game.FireWizardEntity = game.WizardEntity.extend({
         this.parent(x, y, settings);
 
         // setup props
-        this.name = 'Valeriya';
-
-        // {!} EXPR
-        this.renderable.addAnimation('stand_left', [0]);
-        this.renderable.addAnimation('stand_right', [6]);
-        this.renderable.addAnimation('stand_up', [0]);
-        this.renderable.addAnimation('stand_down', [0]);
-        this.renderable.addAnimation('walk_up', [0, 1, 2, 3, 4, 5]);
-        this.renderable.addAnimation('walk_left', [12, 13, 14, 15, 16, 17]);
-        this.renderable.addAnimation('walk_down', [0, 1, 2, 3, 4, 5]);
-        this.renderable.addAnimation('walk_right', [6, 7, 8, 9, 10, 11]);                
+        this.name = 'Valeriya';            
 
         this.playAnimation('stand_left');
     }
