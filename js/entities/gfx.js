564d295bdd94d9cfbadee926e23bb67a863db9f7
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-12-01 16:52:45 +0100

    Fixed canvas refresh bug on iPad devices

--- js/entities/gfx.js
+++ js/entities/gfx.js
@@ -87,13 +87,23 @@ game.GFX.SpellEntity = me.ObjectEntity.extend({
  */
 game.GFX.ClearScreen = me.Renderable.extend({
     init: function() {
-        this.parent(new me.Vector2d(0, 0), me.video.getWidth(), me.video.getHeight());
+        // var w = me.device.isMobile ? Math.floor(me.video.getWidth() * 1.99) : me.video.getWidth();
+        // var h = me.device.isMobile ? Math.floor(me.video.getHeight() * 1.99) : me.video.getHeight();
+        var w = me.video.getWidth();
+        var h = me.video.getHeight();
+        this.parent(new me.Vector2d(0, 0), w, h);
         this.z = -Infinity;
         this.isPersistent = true;
         this.visible = true;
     },
 
     update: function() {
+        // iOS 6 canvas bugfix
+        // http://blog.jackadam.net/2010/the-unfortunate-state-of-canvas-animations-on-the-iphone-ipad/
+        if (me.device.isMobile) {
+            var canvas = me.video.getScreenCanvas();
+            canvas.width = canvas.width + 0;
+        }
         return true;
     },
 
