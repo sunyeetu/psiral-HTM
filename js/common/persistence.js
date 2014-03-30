316eb5bcfdc9d20b5a86b373e1f24a447075bc04
Petar Petrov <petar.petrov.georgiev@gmail.com>
2014-03-31 00:14:38 +0200

    Added persistence skeleton

---
+++ js/common/persistence.js
@@ -0,0 +1,27 @@
+/**
+ * persistence.js
+ *
+ * Copyright (c) 2014 Dvubuz Games
+ *
+ */
+
+
+(function Persistence(w)) {
+
+
+	var _instance = {
+
+		SOUND: "sound",
+		MUSIC: "music",
+
+		get: function(key, default) {
+			//TODO:
+		},
+
+		set: function(key, value) {
+			//TODO:
+		}
+
+	};
+	w.persistence = _instance;
+}(window);
