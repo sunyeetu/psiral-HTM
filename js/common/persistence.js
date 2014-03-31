229ac81ed0d02940ec7a32c0b50decf679f02841
Petar Petrov <petar.petrov.georgiev@gmail.com>
2014-03-31 17:35:34 +0200

    Added functionality to persistence API

--- js/common/persistence.js
+++ js/common/persistence.js
@@ -5,23 +5,70 @@
  *
  */
 
-
 (function Persistence(w)) {
 
+    var ls = w.localStorage;
+    var enabled = !!ls;
 
-	var _instance = {
+    var _instance = {
+        /**
+         * Constants
+         */
+        _KEY: "psidata",
+        SOUND: "sound",
+        MUSIC: "music",
 
-		SOUND: "sound",
-		MUSIC: "music",
+        /**
+         * Private
+         */
+        _autocommit: false,
 
-		get: function(key, default) {
-			//TODO:
-		},
+        _data: null,
 
-		set: function(key, value) {
-			//TODO:
-		}
+        init: function() {
+            if (!enabled) {
+                this.reset();
+                return;
+            }
 
-	};
-	w.persistence = _instance;
+            this.data = ls[_KEY];
+            if (!this.data) {
+                this.reset();
+            }
+        },
+
+        reset: function() {
+            this.data = {
+                // defaults ?
+            };
+        },
+
+        get: function(key, defValue) {
+            var value = this.data[key];
+            return value !== null ? value : defValue;
+        },
+
+        set: function(key, value) {
+            if (!enabled) return;
+
+            this.data[key] = value;
+            
+            if (this._autocommit)
+                this.commit();
+
+            return this;
+        },
+
+        commit: function() {
+            if (!enabled) return;
+
+            ls.setItem(this._KEY, JSON.stringify(this.data));
+            return this;
+        }
+    };
+    Object.defineProperty(_instance, 'autoCommit', {
+        get: function() { return _instance._autocommit; },
+        set: function(autoCommit) { _instance._autocommit = autoCommit; }
+    }); 
+    w.persistence = _instance;
 }(window);
